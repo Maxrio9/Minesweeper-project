9bc8880a4c8291bd4baea90951fe2e6c26b4ba3f
Maxrio <lasse@rosendahl-energietechnik.de>
2024-04-17 09:04:09 +0200

    reorganized the code and added interaction chaning the revealed state

--- logic.py
+++ logic.py
@@ -1,10 +1,16 @@
 from random import randrange
+import math
 import pygame
 
 # Define base variables
 bombs = 99;
 rows = 16;
 columns = 30;
+
+# Display variables
+sprite_size = 17;
+display_scale = 2;
+top_border = 40;
 
 # Create the blank field with (bomb, revealed, neighbours)
 
@@ -57,27 +63,62 @@ for row in range(rows):
                     neighbours += 1;
         field[row][column][2] = neighbours;
 
+# Calculate cell in field from mouse position
+def pos_field(pos):
+    x = pos[0]
+    y = pos[1]
+    column = math.floor(x / (sprite_size * display_scale))
+    row = math.floor((y - top_border) / (sprite_size * display_scale))
+    return column, row
+
+# Update the values of the field after click
+
+def update(pos):
+    print(pos)
+    column, row = pos_field(pos)
+    print(column, row)
+    field[row][column][1] = 1;
+    
+
 # Visulization and Input
+
 
 # pygame setup
 pygame.init();
-screen = pygame.display.set_mode((1080, 720));
+
+screen_width = sprite_size * display_scale * columns;
+screen_height = sprite_size * display_scale * rows + top_border;
+
+screen = pygame.display.set_mode((screen_width, screen_height));
 clock = pygame.time.Clock();
 running = True;
-dt = 0;
-screen_width = screen.get_width();
-screen_height = screen.get_height();
-
-grid_rows = rows;
-grid_columns = columns;
-sprite_size = 17;
-display_scale = 2;
 
 # sprites
 blank_img = pygame.image.load("sprites/blank.png")
 blank_img = pygame.transform.scale(blank_img, (sprite_size * display_scale, sprite_size * display_scale))
 rev0_img = pygame.image.load("sprites/rev0.png")
 rev0_img = pygame.transform.scale(rev0_img, (sprite_size * display_scale, sprite_size * display_scale))
+rev1_img = pygame.image.load("sprites/rev1.png")
+rev1_img = pygame.transform.scale(rev1_img, (sprite_size * display_scale, sprite_size * display_scale))
+rev2_img = pygame.image.load("sprites/rev2.png")
+rev2_img = pygame.transform.scale(rev2_img, (sprite_size * display_scale, sprite_size * display_scale))
+rev3_img = pygame.image.load("sprites/rev3.png")
+rev3_img = pygame.transform.scale(rev3_img, (sprite_size * display_scale, sprite_size * display_scale))
+rev4_img = pygame.image.load("sprites/rev4.png")
+rev4_img = pygame.transform.scale(rev4_img, (sprite_size * display_scale, sprite_size * display_scale))
+rev5_img = pygame.image.load("sprites/rev5.png")
+rev5_img = pygame.transform.scale(rev5_img, (sprite_size * display_scale, sprite_size * display_scale))
+rev6_img = pygame.image.load("sprites/rev6.png")
+rev6_img = pygame.transform.scale(rev6_img, (sprite_size * display_scale, sprite_size * display_scale))
+rev7_img = pygame.image.load("sprites/rev7.png")
+rev7_img = pygame.transform.scale(rev7_img, (sprite_size * display_scale, sprite_size * display_scale))
+rev8_img = pygame.image.load("sprites/rev8.png")
+rev8_img = pygame.transform.scale(rev8_img, (sprite_size * display_scale, sprite_size * display_scale))
+bomb_img = pygame.image.load("sprites/bomb.png")
+bomb_img = pygame.transform.scale(bomb_img, (sprite_size * display_scale, sprite_size * display_scale))
+bomb_exp_img = pygame.image.load("sprites/bomb_exp.png")
+bomb_exp_img = pygame.transform.scale(bomb_exp_img, (sprite_size * display_scale, sprite_size * display_scale))
+
 
 class Blank(pygame.sprite.Sprite):
     def __init__(self, column, row):
@@ -87,11 +128,99 @@ class Blank(pygame.sprite.Sprite):
         self.rect.y = row
         self.rect.x = column
 
+class Rev0(pygame.sprite.Sprite):
+    def __init__(self, column, row):
+        super().__init__()
+        self.image = rev0_img
+        self.rect = self.image.get_rect()
+        self.rect.y = row
+        self.rect.x = column
+
+class Rev1(pygame.sprite.Sprite):
+    def __init__(self, column, row):
+        super().__init__()
+        self.image = rev1_img
+        self.rect = self.image.get_rect()
+        self.rect.y = row
+        self.rect.x = column
+
+class Rev2(pygame.sprite.Sprite):
+    def __init__(self, column, row):
+        super().__init__()
+        self.image = rev2_img
+        self.rect = self.image.get_rect()
+        self.rect.y = row
+        self.rect.x = column
+
+class Rev3(pygame.sprite.Sprite):
+    def __init__(self, column, row):
+        super().__init__()
+        self.image = rev3_img
+        self.rect = self.image.get_rect()
+        self.rect.y = row
+        self.rect.x = column
+
+class Rev4(pygame.sprite.Sprite):
+    def __init__(self, column, row):
+        super().__init__()
+        self.image = rev4_img
+        self.rect = self.image.get_rect()
+        self.rect.y = row
+        self.rect.x = column
+
+class Rev5(pygame.sprite.Sprite):
+    def __init__(self, column, row):
+        super().__init__()
+        self.image = rev5_img
+        self.rect = self.image.get_rect()
+        self.rect.y = row
+        self.rect.x = column
+
+class Rev6(pygame.sprite.Sprite):
+    def __init__(self, column, row):
+        super().__init__()
+        self.image = rev6_img
+        self.rect = self.image.get_rect()
+        self.rect.y = row
+        self.rect.x = column
+
+class Rev7(pygame.sprite.Sprite):
+    def __init__(self, column, row):
+        super().__init__()
+        self.image = rev7_img
+        self.rect = self.image.get_rect()
+        self.rect.y = row
+        self.rect.x = column
+
+class Rev8(pygame.sprite.Sprite):
+    def __init__(self, column, row):
+        super().__init__()
+        self.image = rev8_img
+        self.rect = self.image.get_rect()
+        self.rect.y = row
+        self.rect.x = column
+
+class Bomb(pygame.sprite.Sprite):
+    def __init__(self, column, row):
+        super().__init__()
+        self.image = bomb_img
+        self.rect = self.image.get_rect()
+        self.rect.y = row
+        self.rect.x = column
+
+class Bomb_exp(pygame.sprite.Sprite):
+    def __init__(self, column, row):
+        super().__init__()
+        self.image = bomb_exp_img
+        self.rect = self.image.get_rect()
+        self.rect.y = row
+        self.rect.x = column
+
 sprites_group = pygame.sprite.Group()
 for row in range(rows):
     for col in range(columns):
         x = col * sprite_size * display_scale
-        y = row * sprite_size * display_scale
+        y = row * sprite_size * display_scale + top_border
         blank = Blank(x, y)
         sprites_group.add(blank)
 
@@ -101,6 +230,11 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False;
+        elif event.type == pygame.MOUSEBUTTONUP:
+            for sprite in sprites_group:
+                if sprite.rect.collidepoint(event.pos):
+                    update(event.pos)
+                    
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill((65, 65, 65));
